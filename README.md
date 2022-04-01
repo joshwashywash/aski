a340c16798771310d1eaa5fc1a3316527b8f7278
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-03-31 18:51:04 -0700

    add resizing functionality

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Table of Contents
 
-- [Quick Start](#Quick-Start)
-- [What You Should Know](#What-You-Should-Know)
+- [Quick Start](#quick-start)
+- [What You Should Know](#what-you-should-know)
 
 ## Quick Start
 
@@ -31,7 +31,8 @@ $ cat meowth.txt
 
 ## What You Should Know
 
-- The ascii text file is created in the same directory that you run the application in. Be careful.
+- The ascii text file is created in the same directory that you run the application in.
+- If you don't give the output file a name with the `-o` flag, it will default to the same base name as the input image. For example, `meowth.png` will generate `meowth.txt`. Be careful that you don't overwrite any important files this way.
 - There is very basic error handling and I'm sure not everything is accounted for.
-- The images are not resized. If you have a large image, you will have a large text file.
 - The input images are not quantized. The app seems to work best with images that have a small color palette.
+- The image is trimmed to fit it's content. See [sharp's trim function](https://sharp.pixelplumbing.com/api-resize#trim) for the deets.
--- src/index.ts
+++ src/index.ts
@@ -24,15 +24,42 @@ ${chalk.bold('Usage')}:
 
   $ ${name} <input>
 
+${chalk.bold('Options')}
+
+  --height, -h Resize image to match this width before being asciified.
+  --width, -w Resize image to match this width before being asciified.
+  --out, -o Set the file name of the output .txt file.
+
 ${chalk.bold('Examples')}:
 
   $ ${name} meowth.png
-  $ ${name} images/meowth.png
+
+  resize to 60 pixels tall
+  $ ${name} images/meowth.png -h 60
+
+  resize to 100 pixels wide and set the output file name to ascii.txt
+  $ ${name} meowth.png -w 100 -o ascii
 `;
 
 const cli = meow(helpText, {
   importMeta: import.meta,
+  flags: {
+    height: {
+      type: 'number',
+      alias: 'h',
+    },
+    width: {
+      type: 'number',
+      alias: 'w',
+    },
+    out: {
+      type: 'string',
+      alias: 'o',
+    },
+  },
 });
+
+const { width, height, out } = cli.flags;
 
 if (cli.input.length > 0) {
   const [file] = cli.input;
@@ -43,6 +70,7 @@ if (cli.input.length > 0) {
         const { data, info } = await sharp(file)
           .toColorspace('b-w')
           .trim()
+          .resize({ width, height })
           .raw()
           .toBuffer({ resolveWithObject: true });
 
@@ -52,7 +80,7 @@ if (cli.input.length > 0) {
             `${CHARS.at(toCharIndex(p))}${i % info.width === 0 ? '\n' : ''}`
         ).join('');
 
-        const output = `${basename(file, extname(file))}.txt`;
+        const output = `${out ?? basename(file, extname(file))}.txt`;
         await outputFile(output, ascii);
         console.log(
           `Successfully asciified file ${chalk.bold(file)} to ${chalk.bold(
